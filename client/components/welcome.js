0126701f04be6e96d9c366adf4d547d8f6ebdb2b
Dylan Horgan <dyhorgan@gmail.com>
2020-09-23 18:33:50 -0400

    changed welcome page

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -33,14 +33,14 @@ export class Welcome extends React.Component {
                 <p>
                   I have a passion for designing and developing seamless
                   experiences and elegant interfaces. From web designs, to
-                  full-stack mobile applications, I will be a valuable addition
-                  to your next project.{' '}
+                  full-stack applications, I will be a valuable addition to your
+                  next project.{' '}
                 </p>
                 <p>
                   I have just completed FullStack Academy of Code. FullStack is
                   a 17-week full-time software development bootcamp. Its
-                  curriculum covers NodeJS, Express, Relational Databases such
-                  as PostgreSQL, React and Redux.
+                  curriculum covers NodeJS, Express, PostgreSQL, React, and
+                  Redux.
                 </p>
               </div>
               <a
